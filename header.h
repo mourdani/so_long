f0a093f3a8166d9a0183da5e45d2749fd9b67dd4
mourdani <mourdani@student.42.fr>
2021-11-22 04:20:34 +0100

    add structs

--- header.h
+++ header.h
@@ -1,6 +1,12 @@
 #ifndef SO_LONG_H
 # define SO_LONG_H
 
+typedef struct t_map{
+	int	width;
+	int	height;
+	char	*content;
+} t_map;
+
 typedef struct t_image{
 	void	*id;
 	int	height;
@@ -11,14 +17,19 @@ typedef struct t_mlx{
 	void	*id;
 	void	*window;
 	t_image	tile;
+	t_image	skull;
+	t_image	collectible;
+	t_image	player;
 } t_mlx;
 
 #define PATH_TILE "assets/tileset.xpm"
 #define PATH_SKULL "assets/skull.xpm"
 #define PATH_SKULL_END "./assets/skull_end.xpm"
+#define PATH_COLLECTIBLE "./assets/collectible.xpm"
 #define PATH_ALCHIMIST "./assets/alchimist.xpm"
 
 #include "mlx/mlx.h"
 #include "mlx/mlx_int.h"
+#include "src/gnl/get_next_line.h"
 
 #endif
